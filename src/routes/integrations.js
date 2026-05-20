15550f6bb9e3a8f84fc129623a0c261fd8fb00bc
Abdulsalam Albulaihi <abublihi@gmail.com>
2026-05-20 15:45:14 +0300

    feat(routes): integrate Zod body validation middleware

--- src/routes/integrations.js
+++ src/routes/integrations.js
@@ -3,15 +3,20 @@ const router = express.Router();
 const IntegrationController = require("../controllers/integrationController");
 const ImportController = require("../controllers/importController");
 const requireAuth = require("../middleware/auth");
+const {
+  validateBody,
+  integrationCreateSchema,
+  integrationUpdateSchema,
+} = require("../middleware/validator");
 
 // All integration routes require admin authentication
 router.use(requireAuth);
 
-router.post("/", IntegrationController.create);
+router.post("/", validateBody(integrationCreateSchema), IntegrationController.create);
 router.get("/", IntegrationController.list);
 router.get("/:id", IntegrationController.getById);
 router.get("/:id/scenarios", IntegrationController.getScenarios);
-router.put("/:id", IntegrationController.update);
+router.put("/:id", validateBody(integrationUpdateSchema), IntegrationController.update);
 router.delete("/:id", IntegrationController.delete);
 
 // Postman import routes
